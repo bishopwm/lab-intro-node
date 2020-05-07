42a3df3c4d8e931af878c0f66a5aa73480dea2bb
bishopwm <bishopwm@g.cofc.edu>
2020-05-07 12:25:19 -0400

    completed iteration 2 and 3

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ class SortedList {
     this.length = this.items.length;
   }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort();
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= this.items.length){
+      throw new Error('OutOfBounds')
+    } else if (pos < 0){
+      throw new Error('OutOfBounds')
+    } else {
+      return this.items[pos]
+    }
+  }
 
   max() {}
 
